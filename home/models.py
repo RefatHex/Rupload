d7c4f71452fa9b763f74d30f00b114fbe8fce5e8
refat <khondakar00@gmail.com>
2024-01-24 10:18:42 -0700

    added models for file upload

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
+import os
+from uuid import uuid4
 from django.db import models
 
 
+class Folder(models.Model):
+    uid = models.UUIDField(
+        primary_key=True, editable=False, default=uuid4.uuid)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+def get_upload_path(insatance, filename):
+    return os.path.join(str(insatance.folder.uuid), filename)
+
+
+class Files(models.Model):
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
+    file = models.FileField(upload_to=get_upload_path)
+    created_at = models.DateTimeField(auto_now=True)
